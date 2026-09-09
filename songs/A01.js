fabf304fa6a1fa5c9ed4c18d5bf14f06cce07ee3
Squasew <squasewpendragon@gmail.com>
2026-09-09 23:12:52 +0200

    Initial Push

---
+++ songs/A01.js
@@ -0,0 +1,28 @@
+var q = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.9, delay: 7.8}
+  ]
+};
+
+var w = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.9, delay: 11.3}
+  ]
+};
+
+var e = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.9, delay: 15.3}
+  ]
+};
+
+var song = {
+  duration: 17,
+  sheet: [q, w, e]
+};
