989283bc3161f4e65cd989411958817a42c30bd6
Squasew <squasewpendragon@gmail.com>
2026-09-11 15:51:48 +0200

    Ejemplo de canción con aviso pre-hit

---
+++ songs/A02.js
@@ -0,0 +1,39 @@
+var q = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.9, delay: 6.5},
+    { duration: 0.9, delay: 7.5},
+    { duration: 0.9, delay: 8.0}
+  ]
+};
+
+var w = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.9, delay: 9.5},
+    { duration: 0.9, delay: 10.5},
+    { duration: 0.9, delay: 11.5},
+    { duration: 0.9, delay: 12.5}
+  ]
+};
+
+var e = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.9, delay: 14.5},
+    { duration: 0.9, delay: 15.0},
+    { duration: 0.9, delay: 16.0}
+  ]
+};
+
+var song = {
+  id: 'A02',
+  title: 'Paciencia divina',
+  subtitle: 'Sonido pre-hit',
+  audio: 'media/A02.mp3',
+  duration: 18,
+  sheet: [q, w, e]
+};
