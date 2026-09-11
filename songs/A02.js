05793d69ffd4849624ebd85a09b0831fea9b68e3
Squasew <squasewpendragon@gmail.com>
2026-09-11 16:02:01 +0200

    Ajustes de velocidad para pruebas en A02

--- songs/A02.js
+++ songs/A02.js
@@ -2,9 +2,9 @@ var q = {
   color: 'rgba(218, 165, 32, 1)',
   next: 0,
   notes: [
-    { duration: 0.9, delay: 6.5},
-    { duration: 0.9, delay: 7.5},
-    { duration: 0.9, delay: 8.0}
+    { duration: 0.75, delay: 6.5},
+    { duration: 0.75, delay: 7.5},
+    { duration: 0.75, delay: 8.0}
   ]
 };
 
@@ -12,10 +12,10 @@ var w = {
   color: 'rgba(218, 165, 32, 1)',
   next: 0,
   notes: [
-    { duration: 0.9, delay: 9.5},
-    { duration: 0.9, delay: 10.5},
-    { duration: 0.9, delay: 11.5},
-    { duration: 0.9, delay: 12.5}
+    { duration: 0.75, delay: 9.5},
+    { duration: 0.75, delay: 10.5},
+    { duration: 0.75, delay: 11.5},
+    { duration: 0.75, delay: 12.5}
   ]
 };
 
@@ -23,9 +23,9 @@ var e = {
   color: 'rgba(218, 165, 32, 1)',
   next: 0,
   notes: [
-    { duration: 0.9, delay: 14.5},
-    { duration: 0.9, delay: 15.0},
-    { duration: 0.9, delay: 16.0}
+    { duration: 0.75, delay: 14.5},
+    { duration: 0.75, delay: 15.0},
+    { duration: 0.75, delay: 16.0}
   ]
 };
 
