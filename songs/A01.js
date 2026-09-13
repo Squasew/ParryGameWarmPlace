634c6d75ffeb4022a47a59229552f174bd37d6c8
Squasew <squasewpendragon@gmail.com>
2026-09-13 15:55:18 +0200

    Version Real A01 y A02

--- songs/A01.js
+++ songs/A01.js
@@ -2,9 +2,10 @@ var q = {
   color: 'rgba(218, 165, 32, 1)',
   next: 0,
   notes: [
-    { duration: 0.9, delay: 6.5},
-    { duration: 0.9, delay: 7.5},
-    { duration: 0.9, delay: 8.0}
+    { duration: 0.75, delay: 6.5},
+    { duration: 0.75, delay: 7.5},
+    { duration: 0.75, delay: 8.0},
+    { duration: 0.75, delay: 10.5}
   ]
 };
 
@@ -12,9 +13,10 @@ var w = {
   color: 'rgba(218, 165, 32, 1)',
   next: 0,
   notes: [
-    { duration: 0.9, delay: 10.5},
-    { duration: 0.9, delay: 11.5},
-    { duration: 0.9, delay: 12.5}
+    { duration: 0.75, delay: 9.5},
+    { duration: 0.75, delay: 10.5},
+    { duration: 0.75, delay: 11.5},
+    { duration: 0.75, delay: 12.5}
   ]
 };
 
@@ -22,16 +24,17 @@ var e = {
   color: 'rgba(218, 165, 32, 1)',
   next: 0,
   notes: [
-    { duration: 0.9, delay: 14.5},
-    { duration: 0.9, delay: 15.0},
-    { duration: 0.9, delay: 16.0}
+    { duration: 0.75, delay: 11.5},
+    { duration: 0.75, delay: 14.5},
+    { duration: 0.75, delay: 15.0},
+    { duration: 0.75, delay: 16.0}
   ]
 };
 
 var song = {
   id: 'A01',
-  title: 'Paciencia divina',
-  subtitle: 'A warm place',
+  title: 'Éleos gia tous amartoloús',
+  subtitle: 'Este es el primer y último aviso.',
   audio: 'media/A01.mp3',
   duration: 18,
   sheet: [q, w, e]
--- songs/A02.js
+++ songs/A02.js
@@ -2,9 +2,10 @@ var q = {
   color: 'rgba(218, 165, 32, 1)',
   next: 0,
   notes: [
-    { duration: 0.75, delay: 6.5},
-    { duration: 0.75, delay: 7.5},
-    { duration: 0.75, delay: 8.0}
+    { duration: 0.75, delay: 7.0},
+    { duration: 0.75, delay: 9.0},
+    { duration: 0.75, delay: 15.0},
+    { duration: 0.75, delay: 17.0}
   ]
 };
 
@@ -12,10 +13,12 @@ var w = {
   color: 'rgba(218, 165, 32, 1)',
   next: 0,
   notes: [
-    { duration: 0.75, delay: 9.5},
-    { duration: 0.75, delay: 10.5},
-    { duration: 0.75, delay: 11.5},
-    { duration: 0.75, delay: 12.5}
+    { duration: 0.75, delay: 8.0},
+    { duration: 0.75, delay: 9.0},
+    { duration: 0.75, delay: 11.0},
+    { duration: 0.75, delay: 13.0},
+    { duration: 0.75, delay: 15.5},
+    { duration: 0.75, delay: 17.0}
   ]
 };
 
@@ -23,18 +26,17 @@ var e = {
   color: 'rgba(218, 165, 32, 1)',
   next: 0,
   notes: [
-    { duration: 0.75, delay: 9.5},
-    { duration: 0.75, delay: 14.5},
-    { duration: 0.75, delay: 15.0},
+    { duration: 0.75, delay: 12.0},
+    { duration: 0.75, delay: 13.0},
     { duration: 0.75, delay: 16.0},
-    { duration: 0.75, delay: 16.2}
+    { duration: 0.75, delay: 17.0}
   ]
 };
 
 var song = {
   id: 'A02',
-  title: 'Paciencia divina',
-  subtitle: 'Sonido pre-hit',
+  title: 'Éleos gia tous amartoloús',
+  subtitle: 'El angel avisó a los peregrinos de que volvieran por donde habían venido...',
   audio: 'media/A02.mp3',
   duration: 18,
   sheet: [q, w, e]
