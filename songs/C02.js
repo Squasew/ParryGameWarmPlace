0bc7aed30d1bc113405983f7f9ab9bf6b1764664
Squasew <squasewpendragon@gmail.com>
2026-09-13 18:58:29 +0200

    Final de C02

---
+++ songs/C02.js
@@ -0,0 +1,56 @@
+var q = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.75, delay: 6.0},
+    { duration: 0.75, delay: 7.0},
+    { duration: 0.75, delay: 8.0},
+    { duration: 0.75, delay: 11.22},
+    { duration: 0.75, delay: 12.5},
+    { duration: 0.75, delay: 12.72},
+    { duration: 0.75, delay: 13.36},
+    { duration: 0.75, delay: 15.0},
+    { duration: 0.75, delay: 16.0}
+  ]
+};
+
+var w = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.75, delay: 7.0},
+    { duration: 0.75, delay: 8.0},
+    { duration: 0.75, delay: 9.0},
+    { duration: 0.75, delay: 11.0},
+    { duration: 0.75, delay: 11.0},
+    { duration: 0.75, delay: 11.22},
+    { duration: 0.75, delay: 13.36},
+    { duration: 0.75, delay: 14.0},
+    { duration: 0.75, delay: 16.0}
+  ]
+};
+
+var e = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.75, delay: 8.0},
+    { duration: 0.75, delay: 9.0},
+    { duration: 0.75, delay: 10.0},
+    { duration: 0.75, delay: 11.22},
+    { duration: 0.75, delay: 12.93},
+    { duration: 0.75, delay: 13.15},
+    { duration: 0.75, delay: 13.36},
+    { duration: 0.75, delay: 15.5},
+    { duration: 0.75, delay: 16.0}
+  ]
+};
+
+var song = {
+  id: 'C02',
+  title: 'Éleos gia tous amartoloús',
+  subtitle: '¡Está aquí!',
+  audio: 'media/C02.mp3',
+  duration: 18.0,
+  sheet: [q, w, e]
+};
