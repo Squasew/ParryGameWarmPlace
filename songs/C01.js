7944ca51382235a7af64a61bb41298824b9ea7c9
Squasew <squasewpendragon@gmail.com>
2026-09-13 18:36:16 +0200

    C01 completado

--- songs/C01.js
+++ songs/C01.js
@@ -2,11 +2,16 @@ var q = {
   color: 'rgba(218, 165, 32, 1)',
   next: 0,
   notes: [
-    { duration: 0.70, delay: 6.0},
-    { duration: 0.70, delay: 6.5},
-    { duration: 0.70, delay: 9.5},
-    { duration: 0.70, delay: 12.0},
-    { duration: 0.70, delay: 14.5}
+    { duration: 0.75, delay: 6.0},
+    { duration: 0.75, delay: 7.0},
+    { duration: 0.75, delay: 8.0},
+    { duration: 0.75, delay: 9.0},
+    { duration: 0.75, delay: 10.0},
+    { duration: 0.75, delay: 11.0},
+    { duration: 0.75, delay: 12.0},
+    { duration: 0.75, delay: 13.0},
+    { duration: 0.75, delay: 14.0},
+    { duration: 0.75, delay: 15.0}
   ]
 };
 
@@ -14,14 +19,10 @@ var w = {
   color: 'rgba(218, 165, 32, 1)',
   next: 0,
   notes: [
-    { duration: 0.70, delay: 6.5},
-    { duration: 0.70, delay: 7.5},
-    { duration: 0.70, delay: 8.0},
-    { duration: 0.70, delay: 11.5},
-    { duration: 0.70, delay: 12.0},
-    { duration: 0.70, delay: 13.0},
-    { duration: 0.70, delay: 14.0},
-    { duration: 0.70, delay: 15.0}
+    { duration: 0.75, delay: 6.5},
+    { duration: 0.75, delay: 8.0},
+    { duration: 0.75, delay: 11.0},
+    { duration: 0.75, delay: 13.5}
   ]
 };
 
@@ -29,12 +30,10 @@ var e = {
   color: 'rgba(218, 165, 32, 1)',
   next: 0,
   notes: [
-    { duration: 0.70, delay: 8.0},
-    { duration: 0.70, delay: 9.0},
-    { duration: 0.70, delay: 9.5},
-    { duration: 0.70, delay: 13.5},
-    { duration: 0.70, delay: 14.0},
-    { duration: 0.70, delay: 15.5}
+    { duration: 0.75, delay: 7.0},
+    { duration: 0.75, delay: 7.5},
+    { duration: 0.75, delay: 12.0},
+    { duration: 0.75, delay: 14.5}
   ]
 };
 
