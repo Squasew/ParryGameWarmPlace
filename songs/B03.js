b792338e494ce490fb47d98ea86c3a259377094f
Squasew <squasewpendragon@gmail.com>
2026-09-13 18:25:31 +0200

    Todas las B + Inicio de C

---
+++ songs/B03.js
@@ -0,0 +1,47 @@
+var q = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.70, delay: 6.0},
+    { duration: 0.70, delay: 6.79},
+    { duration: 0.70, delay: 7.0},
+    { duration: 0.70, delay: 11.5},
+    { duration: 0.70, delay: 11.72},
+    { duration: 0.70, delay: 15.22}
+  ]
+};
+
+var w = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.70, delay: 7.5},
+    { duration: 0.70, delay: 8.28},
+    { duration: 0.70, delay: 8.5},
+    { duration: 0.70, delay: 12.0},
+    { duration: 0.70, delay: 13.5},
+    { duration: 0.70, delay: 15.0}
+  ]
+};
+
+var e = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.70, delay: 9.0},
+    { duration: 0.70, delay: 9.78},
+    { duration: 0.70, delay: 10.0},
+    { duration: 0.70, delay: 13.0},
+    { duration: 0.70, delay: 13.22},
+    { duration: 0.70, delay: 15.22}
+  ]
+};
+
+var song = {
+  id: 'B03',
+  title: 'Éleos gia tous amartoloús',
+  subtitle: '...pues los pecadores ardeis bajo ella!',
+  audio: 'media/B03.mp3',
+  duration: 16.5,
+  sheet: [q, w, e]
+};
---
+++ songs/C01.js
@@ -0,0 +1,48 @@
+var q = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.70, delay: 6.0},
+    { duration: 0.70, delay: 6.5},
+    { duration: 0.70, delay: 9.5},
+    { duration: 0.70, delay: 12.0},
+    { duration: 0.70, delay: 14.5}
+  ]
+};
+
+var w = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.70, delay: 6.5},
+    { duration: 0.70, delay: 7.5},
+    { duration: 0.70, delay: 8.0},
+    { duration: 0.70, delay: 11.5},
+    { duration: 0.70, delay: 12.0},
+    { duration: 0.70, delay: 13.0},
+    { duration: 0.70, delay: 14.0},
+    { duration: 0.70, delay: 15.0}
+  ]
+};
+
+var e = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.70, delay: 8.0},
+    { duration: 0.70, delay: 9.0},
+    { duration: 0.70, delay: 9.5},
+    { duration: 0.70, delay: 13.5},
+    { duration: 0.70, delay: 14.0},
+    { duration: 0.70, delay: 15.5}
+  ]
+};
+
+var song = {
+  id: 'C01',
+  title: 'Éleos gia tous amartoloús',
+  subtitle: 'El fin... ¡se acerca!',
+  audio: 'media/C01.mp3',
+  duration: 16.5,
+  sheet: [q, w, e]
+};
