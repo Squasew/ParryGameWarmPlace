c31d44a1d089500f0b33a4ab98afa992ed0b4678
Squasew <squasewpendragon@gmail.com>
2026-09-13 16:23:51 +0200

    A03

---
+++ songs/A03.js
@@ -0,0 +1,44 @@
+var q = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.75, delay: 6.0},
+    { duration: 0.75, delay: 10.0},
+    { duration: 0.75, delay: 10.5},
+    { duration: 0.75, delay: 15.0},
+    { duration: 0.75, delay: 16.5}
+  ]
+};
+
+var w = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.75, delay: 7.0},
+    { duration: 0.75, delay: 11.0},
+    { duration: 0.75, delay: 12.0},
+    { duration: 0.75, delay: 12.5},
+    { duration: 0.75, delay: 15.0},
+    { duration: 0.75, delay: 15.5}
+  ]
+};
+
+var e = {
+  color: 'rgba(218, 165, 32, 1)',
+  next: 0,
+  notes: [
+    { duration: 0.75, delay: 8.0},
+    { duration: 0.75, delay: 13.0},
+    { duration: 0.75, delay: 15.0},
+    { duration: 0.75, delay: 16.5}
+  ]
+};
+
+var song = {
+  id: 'A03',
+  title: 'Éleos gia tous amartoloús',
+  subtitle: '...pues su luz no era un faro en la oscuridad sino un aviso de muerte.',
+  audio: 'media/A03.mp3',
+  duration: 18,
+  sheet: [q, w, e]
+};
